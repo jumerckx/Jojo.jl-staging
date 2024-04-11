273b95ecf23218c7637e0abbe15c7ace8e82b29d
jumerckx <julesmerckx12@gmail.com>
2024-04-11 21:12:33 +0200

    use is_intrinsic for inlining_policy. When is_intrinsic rules are redefined, Brutus.reset_cache needs to be called to start using a fresh cache :(

--- Brutus/src/abstract.jl
+++ Brutus/src/abstract.jl
@@ -38,15 +38,21 @@ function MLIRInterpreter(world::UInt = Base.get_world_counter();
                             opt_params, inf_cache)
 end
 
+cache_token = gensym(:MLIRInterpreterCache)
+function reset_cache()
+    global cache_token
+    cache_token = gensym(:MLIRInterpreterCache)
+end
+
 CC.InferenceParams(interp::MLIRInterpreter) = interp.inf_params
 CC.OptimizationParams(interp::MLIRInterpreter) = interp.opt_params
 CC.get_inference_cache(interp::MLIRInterpreter) = interp.inf_cache
 CC.get_inference_world(interp::MLIRInterpreter) = interp.world
-CC.cache_owner(interp::MLIRInterpreter) = nothing
+CC.cache_owner(interp::MLIRInterpreter) = cache_token
 
-# No need to do any locking since we're not putting our results into the runtime cache
-CC.lock_mi_inference(interp::MLIRInterpreter, mi::MethodInstance) = nothing
-CC.unlock_mi_inference(interp::MLIRInterpreter, mi::MethodInstance) = nothing
+# # No need to do any locking since we're not putting our results into the runtime cache
+# CC.lock_mi_inference(interp::MLIRInterpreter, mi::MethodInstance) = nothing
+# CC.unlock_mi_inference(interp::MLIRInterpreter, mi::MethodInstance) = nothing
 
 function CC.add_remark!(interp::MLIRInterpreter, sv::CC.InferenceState, msg)
     @debug "Inference remark during compilation of MethodInstance of $(sv.linfo): $msg"
@@ -71,10 +77,15 @@ function CC.abstract_call_gf_by_type(interp::MLIRInterpreter, @nospecialize(f), 
 
     cm = @invoke CC.abstract_call_gf_by_type(interp::CC.AbstractInterpreter, f::Any,
     arginfo::CC.ArgInfo, si::CC.StmtInfo, atype::Any, sv::CC.InferenceState, max_methods::Int)
-        
-    argtype_tuple = Tuple{map(_type, arginfo.argtypes)...}
     
-    if is_primitive(argtype_tuple)
+    argtype_tuple = try
+        Tuple{map(_type, arginfo.argtypes)...}
+    catch
+        @warn arginfo.argtypes
+        error("stop")
+    end
+    
+    if is_intrinsic(argtype_tuple)
         return CC.CallMeta(cm.rt, cm.exct, cm.effects, MLIRIntrinsicCallInfo(cm.info))
     else
         return cm
@@ -93,18 +104,14 @@ _typeof(x::NamedTuple{names}) where {names} = NamedTuple{names, _typeof(Tuple(x)
 
 _type(x) = x
 _type(x::CC.Const) = _typeof(x.val)
-_type(x::CC.PartialStruct) = x.typ
-_type(x::CC.Conditional) = Union{x.thentype, x.elsetype}
+_type(x::CC.PartialStruct) = _type(x.typ)
+_type(x::CC.Conditional) = Union{_type(x.thentype), _type(x.elsetype)}
 
-is_primitive(::Any) = false
+is_intrinsic(::Any) = false
 
-macro is_primitive(sig)
-    return esc(:(Brutus.is_primitive(::Type{<:$sig}) = true))
+macro is_intrinsic(sig)
+    return esc(:(Brutus.is_intrinsic(::Type{<:$sig}) = true))
 end
-
-is_primitive(::Type{<:Tuple{typeof(*), Integer, Integer}}) = true
-
-# @is_primitive Tuple{typeof(*), Int, Int}
 
 function CC.inlining_policy(interp::MLIRInterpreter,
     @nospecialize(src), @nospecialize(info::CC.CallInfo), stmt_flag::UInt32)
@@ -169,78 +176,78 @@ end
 
 # create a MethodError from a function type
 # TODO: fix upstream
-# function unsafe_function_from_type(ft::Type)
-#     if isdefined(ft, :instance)
-#         ft.instance
-#     else
-#         # HACK: dealing with a closure or something... let's do somthing really invalid,
-#         #       which works because MethodError doesn't actually use the function
-#         Ref{ft}()[]
-#     end
-# end
-# function MethodError(ft::Type{<:Function}, tt::Type, world::Integer=typemax(UInt))
-#     Base.MethodError(unsafe_function_from_type(ft), tt, world)
-# end
-# MethodError(ft, tt, world=typemax(UInt)) = Base.MethodError(ft, tt, world)
+function unsafe_function_from_type(ft::Type)
+    if isdefined(ft, :instance)
+        ft.instance
+    else
+        # HACK: dealing with a closure or something... let's do somthing really invalid,
+        #       which works because MethodError doesn't actually use the function
+        Ref{ft}()[]
+    end
+end
+function MethodError(ft::Type{<:Function}, tt::Type, world::Integer=typemax(UInt))
+    Base.MethodError(unsafe_function_from_type(ft), tt, world)
+end
+MethodError(ft, tt, world=typemax(UInt)) = Base.MethodError(ft, tt, world)
 
-# import Core.Compiler: retrieve_code_info, maybe_validate_code, InferenceState, InferenceResult
-# # Replace usage sites of `retrieve_code_info`, OptimizationState is one such, but in all interesting use-cases
-# # it is derived from an InferenceState. There is a third one in `typeinf_ext` in case the module forbids inference.
-# function InferenceState(result::InferenceResult, cache_mode::UInt8, interp::MLIRInterpreter)
-#     src = retrieve_code_info(result.linfo, interp.world)
-#     src === nothing && return nothing
-#     maybe_validate_code(result.linfo, src, "lowered")
-#     src = transform(interp, result.linfo, src)
-#     maybe_validate_code(result.linfo, src, "transformed")
+import Core.Compiler: retrieve_code_info, maybe_validate_code, InferenceState, InferenceResult
+# Replace usage sites of `retrieve_code_info`, OptimizationState is one such, but in all interesting use-cases
+# it is derived from an InferenceState. There is a third one in `typeinf_ext` in case the module forbids inference.
+function InferenceState(result::InferenceResult, cache_mode::UInt8, interp::MLIRInterpreter)
+    src = retrieve_code_info(result.linfo, interp.world)
+    src === nothing && return nothing
+    maybe_validate_code(result.linfo, src, "lowered")
+    src = transform(interp, result.linfo, src)
+    maybe_validate_code(result.linfo, src, "transformed")
 
-#     return InferenceState(result, src, cache_mode, interp)
-# end
+    return InferenceState(result, src, cache_mode, interp)
+end
 
-# struct DestinationOffsets
-#     indices::Vector{Int}
-#     DestinationOffsets() = new([])
-# end
-# function Base.insert!(d::DestinationOffsets, insertion::Int)
-#     candidateindex = d[insertion]+1
-#     if (length(d.indices) == 0)
-#         push!(d.indices, insertion)
-#     elseif candidateindex == length(d.indices)+1
-#         push!(d.indices, insertion)
-#     elseif (candidateindex == 1) || (d.indices[candidateindex-1] != insertion)
-#         insert!(d.indices, candidateindex, insertion)
-#     end
-#     return d
-# end
-# Base.getindex(d::DestinationOffsets, i::Int) = searchsortedlast(d.indices, i, lt= <=)
+struct DestinationOffsets
+    indices::Vector{Int}
+    DestinationOffsets() = new([])
+end
+function Base.insert!(d::DestinationOffsets, insertion::Int)
+    candidateindex = d[insertion]+1
+    if (length(d.indices) == 0)
+        push!(d.indices, insertion)
+    elseif candidateindex == length(d.indices)+1
+        push!(d.indices, insertion)
+    elseif (candidateindex == 1) || (d.indices[candidateindex-1] != insertion)
+        insert!(d.indices, candidateindex, insertion)
+    end
+    return d
+end
+Base.getindex(d::DestinationOffsets, i::Int) = searchsortedlast(d.indices, i, lt= <=)
 
-# function insert_bool_conversions_pass(mi, src)
-#     offsets = DestinationOffsets()
+function insert_bool_conversions_pass(mi, src)
+    offsets = DestinationOffsets()
 
-#     b = CodeInfoTools.Builder(src)
-#     for (v, st) in b
-#         if st isa Core.GotoIfNot
-#             arg = st.cond isa Core.SSAValue ? var(st.cond.id + offsets[st.cond.id]) : st.cond
-#             b[v] = Statement(Expr(:call, GlobalRef(Brutus, :mlir_bool_conversion), arg))
-#             push!(b, Core.GotoIfNot(v, st.dest))
-#             insert!(offsets, v.id)
-#         elseif st isa Core.GotoNode
-#             b[v] = st
-#         end
-#     end
+    b = CodeInfoTools.Builder(src)
+    for (v, st) in b
+        if st isa Core.GotoIfNot
+            arg = st.cond isa Core.SSAValue ? var(st.cond.id + offsets[st.cond.id]) : st.cond
+            b[v] = Statement(Expr(:call, GlobalRef(Brutus, :mlir_bool_conversion), arg))
+            push!(b, Core.GotoIfNot(v, st.dest))
+            insert!(offsets, v.id)
+        elseif st isa Core.GotoNode
+            b[v] = st
+        end
+    end
 
-#     # fix destinations and conditions
-#     for i in 1:length(b.to)
-#         st = b.to[i].node
-#         if st isa Core.GotoNode
-#             b.to[i] = Core.GotoNode(st.label + offsets[st.label])
-#         elseif st isa Core.GotoIfNot
-#             b.to[i] = Statement(Core.GotoIfNot(st.cond, st.dest + offsets[st.dest]))
-#         end
-#     end
-#     finish(b)
-# end
+    # fix destinations and conditions
+    for i in 1:length(b.to)
+        st = b.to[i].node
+        if st isa Core.GotoNode
+            b.to[i] = Core.GotoNode(st.label + offsets[st.label])
+        elseif st isa Core.GotoIfNot
+            b.to[i] = Statement(Core.GotoIfNot(st.cond, st.dest + offsets[st.dest]))
+        end
+    end
+    finish(b)
+end
 
-# function transform(interp, mi, src)
-#     src = insert_bool_conversions_pass(mi, src)
-#     return src
-# end
+function transform(interp, mi, src)
+    src = insert_bool_conversions_pass(mi, src)
+    return src
+end
